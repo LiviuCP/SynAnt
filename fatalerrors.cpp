beeba67ce15a7bb10f02f49a4453e5d76dbe6b3b
Liviu Popa <liviucst@gmail.com>
2018-07-09 09:17:08 +0100

    Renamed a few variables in the constructor of FatalErrors

--- fatalerrors.cpp
+++ fatalerrors.cpp
@@ -16,23 +16,23 @@ FatalErrors::FatalErrors(QWidget *parent)
     m_ErrorMessage -> setFrameStyle(QFrame::StyledPanel);
     messageLayout -> addWidget(m_ErrorMessage,1);
 
-    QHBoxLayout *okButtonLayout{new QHBoxLayout{}};
-    QPushButton *okButton{new QPushButton{GameStrings::c_FatalErrorQuitButtonLabel}};
-    okButton -> setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    okButton -> setToolTip(GameStrings::c_FatalErrorQuitButtonToolTip);
-    QShortcut *okButtonShortcut{new QShortcut{QKeySequence{GameStrings::c_FatalErrorQuitButtonShortcut},this}};
-    QShortcut *okButtonAltShortcut{new QShortcut{QKeySequence{GameStrings::c_FatalErrorQuitButtonAltShortcut},this}};
-    okButtonLayout -> addWidget(okButton,1);
+    QHBoxLayout *closeButtonLayout{new QHBoxLayout{}};
+    QPushButton *closeButton{new QPushButton{GameStrings::c_FatalErrorQuitButtonLabel}};
+    closeButton -> setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    closeButton -> setToolTip(GameStrings::c_FatalErrorQuitButtonToolTip);
+    QShortcut *closeButtonShortcut{new QShortcut{QKeySequence{GameStrings::c_FatalErrorQuitButtonShortcut},this}};
+    QShortcut *closeButtonAltShortcut{new QShortcut{QKeySequence{GameStrings::c_FatalErrorQuitButtonAltShortcut},this}};
+    closeButtonLayout -> addWidget(closeButton,1);
 
     QVBoxLayout *mainLayout{new QVBoxLayout{}};
     mainLayout -> addLayout(messageLayout,1);
-    mainLayout -> addLayout(okButtonLayout);
+    mainLayout -> addLayout(closeButtonLayout);
     setLayout(mainLayout);
     setToolTip(GameStrings::c_FatalErrorWindowToolTip);
 
-    connect(okButton,&QPushButton::clicked,qApp,&QApplication::quit);
-    connect(okButtonShortcut,&QShortcut::activated,qApp,&QApplication::quit);
-    connect(okButtonAltShortcut,&QShortcut::activated,qApp,&QApplication::quit);
+    connect(closeButton,&QPushButton::clicked,qApp,&QApplication::quit);
+    connect(closeButtonShortcut,&QShortcut::activated,qApp,&QApplication::quit);
+    connect(closeButtonAltShortcut,&QShortcut::activated,qApp,&QApplication::quit);
 }
 
 void FatalErrors::setFatalErrorText(const QString &text)
