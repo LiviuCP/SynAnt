55398b47f968d8447f8e20a29455ac3ad24420a4
Liviu Popa <liviucst@gmail.com>
2018-09-06 08:05:59 +0100

    Removed the style strings from the Common library
    This was required as they were specific to the qWidgets version and a
    separate header file had been created for them

--- Common/gamestrings.h
+++ Common/gamestrings.h
@@ -196,22 +196,6 @@ namespace GameStrings {
     const QString c_MainWindowTitle                     {    "SynAnt - Let's Play!"                                                                     };
     const QString c_FatalErrorWindowTitle               {    "Fatal Error!"                                                                             };
 
-    // styles
-    const QString c_ApplicationStyle                    {    "QLabel,"
-                                                             "QGroupBox{background-color: grey; color: white; border: 1px solid white;}"
-                                                             "QPushButton{background-color: darkgrey; color: white;}"
-                                                             "QRadioButton{background-color: grey; color: white;}"
-                                                             "QToolTip{background-color: grey; color: yellow; border: 1px solid yellow;}"
-                                                             "IntroductionWindow,"
-                                                             "HintsWindow,"
-                                                             "MainGameWindow,"
-                                                             "FatalErrors{background-color: grey;}"
-                                                        };
-
-    const QString c_WordBeginPieceStyle                 {    "color: blue; border-width: 1px;"                                                          };
-    const QString c_WordEndPieceStyle                   {    "color: red; border-width: 1px;"                                                           };
-    const QString c_WordEachPieceStyle                  {    "border-width:1px"                                                                         };
-
     // miscellaneous
     const QString c_FirstWordLowerCase                  {    "first"                                                                                    };
     const QString c_FirstWordCamelCase                  {    "First"                                                                                    };
