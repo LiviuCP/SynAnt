39c75da105a4598f5391c173da87fd291f9cc057
Liviu Popa <liviucst@gmail.com>
2019-08-21 07:27:54 +0100

    Corrected one status message

--- Application/gamepresenter.cpp
+++ Application/gamepresenter.cpp
@@ -776,7 +776,7 @@ void GamePresenter::_onStatusChanged(Game::StatusCodes statusCode)
         break;
     case Game::StatusCodes::DATA_GOT_AVAILABLE:
         Q_EMIT dataEntrySucceeded();
-        _updateStatusMessage(GameStrings::c_DataEntrySuccessMessage.arg(m_pGameFacade->getLastSavedNrOfWordPairs()), Pane::DATA_ENTRY, Game::c_NoDelay);
+        _updateStatusMessage(GameStrings::c_DataSuccessfullySavedMessage.arg(m_pGameFacade->getLastSavedNrOfWordPairs()), Pane::DATA_ENTRY, Game::c_NoDelay);
         _updateStatusMessage(GameStrings::c_DataAvailableMessage, Pane::INTRO, Game::c_NoDelay);
         _updateStatusMessage(GameStrings::c_DataEntryRequestMessage, Pane::DATA_ENTRY, Game::c_ShortStatusUpdateDelay);
         break;
